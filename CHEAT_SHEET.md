5ad33659906b25164bb9b882c05dc3efbccd85ee
Arsalan Sefidgar <a.sefidgar@pep.co.ir>
2024-01-21 11:08:58 +0330

    init

--- CHEAT_SHEET.md
+++ CHEAT_SHEET.md
@@ -1,29 +1,39 @@
+## connecting using mysql cli
+```sh
+mysql -h 127.0.0.1 -P33306 -uroot -p'my_root_password'
+mysql -h 127.0.0.1 -P33306 -uroot -p'my_root_password' -E --execute='show binary logs;' mysql
+```
+
+## initial database and tables
 ```sql
 SHOW databases;
-CREATE DATABASE testDB;
-use testDB;
+CREATE DATABASE my_database;
+use my_database;
 CREATE TABLE Persons (id int NOT NULL AUTO_INCREMENT, FirstName varchar(255), PRIMARY KEY (id));
 SHOW tables;
 INSERT INTO Persons (FirstName) VALUES ('arsalan');
 SELECT * FROM Persons;
 ```
 
+## Restore From Binary Logs
+we first need to restore the most recent full backup,
+We do this by using the `mysqlbinlog` utility to view the log data for the specific database,
+and piping that output to the mysql utility for the database in question.
 ```sh
 find / -type f -name mysql.cnf
-mysqlbinlog --no-defaults /bitnami/mysql/data/mysql-bin.000001
-mysqlbinlog --no-defaults -dturnipjuice /bitnami/mysql/data/mysql-bin.000002
+
+mysqlbinlog --no-defaults -dmy_database /bitnami/mysql/data/mysql-bin.000002
+
+mysqlbinlog --no-defaults -dmy_database ./backups/log_files/26-01-2022_14-44-46.mysql-bin | mysql -uroot -p my_database
 ```
 
+## manual log purge
 ```sql
 show binary logs;
 FLUSH BINARY LOGS;
 PURGE BINARY LOGS TO 'mysql-bin.000003';
 
-show variables like 'server_id';
 show variables like 'expire_logs_days';
 show variables like 'binlog_format';
-```
-
-```sh
-mysql -u$DB_USER -p$DB_PASSWORD -E --execute='show binary logs;' mysql
+show variables like 'server_id';
 ```
--- incremental_backup/script.sh
+++ incremental_backup/script.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+CONTAINER_ID=mysql-high-available-mysql-master-1
+
 # set up the date variable
 NOW=$(date +%Y%m%d%H%M%S)
 BINLOG_BACKUP=${NOW}_binlog.tar.gz
 
 # set up the database credentials
 DB_USER=root
-DB_PASSWORD=root_password
+DB_PASSWORD=my_root_password
 
 # binary log files directory path
-BINLOGS_PATH=/var/log/mysql/
+BINLOGS_PATH=/bitnami/mysql/data/
 
 # flush the current log and start writing to a new binary log file
-mysql -u$DB_USER -p$DB_PASSWORD -E --execute='FLUSH BINARY LOGS;' mysql
+mysql -h 127.0.0.1 -P 33306 -u$DB_USER -p$DB_PASSWORD -E --execute='FLUSH BINARY LOGS;' mysql
 
 # get a list of all binary log files
-BINLOGS=$(mysql -u$DB_USER -p$DB_PASSWORD -E --execute='SHOW BINARY LOGS;' mysql | grep Log_name | sed -e 's/Log_name://g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+BINLOGS=$(mysql -h 127.0.0.1 -P 33306 -u$DB_USER -p$DB_PASSWORD -E --execute='SHOW BINARY LOGS;' mysql | grep Log_name | sed -e 's/Log_name://g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
 # get the most recent binary log file
 BINLOG_CURRENT=`echo "${BINLOGS}" | tail -n -1`
@@ -26,8 +28,12 @@ BINLOGS_FOR_BACKUP=`echo "${BINLOGS}" | head -n -1`
 # create a list of the full paths to the binary logs to be backed up
 BINLOGS_FULL_PATH=`echo "${BINLOGS_FOR_BACKUP}" | xargs -I % echo $BINLOGS_PATH%`
 
+for LOG_FILE in $BINLOGS_FULL_PATH; do docker cp "$CONTAINER_ID:$LOG_FILE" "./log_files/"; done
+
+BINLOGS_FULL_PATH=`echo "${BINLOGS_FOR_BACKUP}" | xargs -I % echo "./log_files/"%`
+
 # compress the list of binary logs to be backed up into an archive in the backup location
-tar -czvf /sites/backups/$BINLOG_BACKUP $BINLOGS_FULL_PATH
+tar -czvf ./backups/$BINLOG_BACKUP $BINLOGS_FULL_PATH
 
 # delete the binary logs that have been backed up
-echo $BINLOG_CURRENT | xargs -I % mysql -u$DB_USER -p$DB_PASSWORD -E --execute='PURGE BINARY LOGS TO "%";' mysql
+echo $BINLOG_CURRENT | xargs -I % mysql -h 127.0.0.1 -P 33306 -u$DB_USER -p$DB_PASSWORD -E --execute='PURGE BINARY LOGS TO "%";' mysql
